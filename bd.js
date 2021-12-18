f3ba3218f9ce939a200f1a53eb60cc6e30abaea8
IagoCB <iago.luiz@ges.inatel.br>
2021-12-18 20:16:29 -0300

    Implementação das funções de inserir e buscar um usuário no banco de dados

--- bd.js
+++ bd.js
@@ -1,13 +1,28 @@
+//função assíncrona de conexão com o banco de dados local
 async function connect(){
     if(global.connection && global.connection.state !== 'disconnected')
         return global.connection;
 
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection("mysql://root:root@localhost:3306/mydb");
-    console.log("Conectou no MySQL")
+    const connection = await mysql.createConnection("mysql://root:root@localhost:3306/mydb");    
     global.connection = connection;
     return connection;
 }
-connect();
 
-module.exports = {}
+//função assíncrona para inserção de um usuário no banco de dados
+async function insertUser(user){
+    const conn = await connect();
+    const sql = 'INSERT INTO usuario(CPF, nome, email, endereco, telefone) VALUES (?,?,?,?,?);';
+    const values = [user.cpf, user.nome, user.email, user.endereco, user.telefone];
+    await conn.query(sql, values);
+}
+
+//função assíncrona para buscar um usuário no banco de dados
+async function selectUser(cpf_select){
+    const conn = await connect();
+    const [rows] = await conn.query('SELECT * FROM usuario WHERE CPF ='+ cpf_select +';'); 
+    return rows;    
+}
+
+//exportando módulos
+module.exports = {insertUser, selectUser}
--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-const db = require("./bd")
-console.log("Começou");
+//criando uma função assíncrona genérica
+(async () => {  
+  //chamando a conecxao com o banco de dados
+  const db = require("./bd")
+  console.log("Começou");
+
+  //criando uma constante e fazendo uma requisição de um pacote para a entrada de dados 
+  const entrada = require('prompt-sync')({sigint: true});
+    
+  //inserindo um usuário
+  console.log("Vamos inserir um usuario!");
+  var cpf = entrada('Digite o cpf(somente números): ');
+  var nome = entrada("Digite o nome: ");
+  var email = entrada("Digite o email: ");
+  var endereco = entrada("Digite o endereco: ");
+  var telefone = entrada("Digite o telefone: ");
+  await db.insertUser({cpf: cpf, nome:  nome, email: email, endereco: endereco, telefone:  telefone});
+
+  //buscando um usuário
+  console.log("Vamos buscar um usuario!");
+  var cpf_select = entrada("Digite um CPF: ");
+  var user = await db.selectUser(cpf_select);    
+  console.log(user);
+  
+})();
