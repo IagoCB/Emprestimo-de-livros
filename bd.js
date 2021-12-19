12165ca51ffddaa499fbc6de1d0a03cc30c3884c
IagoCB <iago.luiz@ges.inatel.br>
2021-12-19 15:00:55 -0300

    Implementação das funções de cadastrar livro, realizar empréstimo e a ligação entre elas

--- bd.js
+++ bd.js
@@ -24,5 +24,29 @@ async function selectUser(cpf_select){
     return rows;    
 }
 
+//função assíncrona para inserção de um livro no banco de dados
+async function insertLivro(book){
+    const conn = await connect();
+    const sql = 'INSERT INTO livro(titulo, autor, USUARIO_CPF) VALUES (?,?,?);';
+    const values = [book.titulo, book.autor, book.user_cpf];
+    await conn.query(sql, values);
+}
+
+//função assíncrona para inserção de um emprestimo no banco de dados
+async function insertEmp(emp){
+    const conn = await connect();
+    const sql = 'INSERT INTO EMPRESTIMO(retirada, devolucao, USUARIO_CPF) VALUES (?,?,?);';
+    const values = [emp.retirada, emp.devolucao, emp.user_cpf];
+    await conn.query(sql, values);
+}
+
+//função assíncrona para inserção de um emprestimo no banco de dados
+async function insertLigacao(lig){
+    const conn = await connect();
+    const sql = 'INSERT INTO EMPRESTIMO_LIVRO(EMPRESTIMO_idEMPRESTIMO, EMPRESTIMO_USUARIO_CPF, LIVRO_idLIVRO, LIVRO_USUARIO_CPF) VALUES (?,?,?,?);';
+    const values = [lig.idEMP, lig.cpfEMP, lig.idLIVRO, lig.cpfLIVRO];
+    await conn.query(sql, values);
+}
+
 //exportando módulos
-module.exports = {insertUser, selectUser}
+module.exports = {insertUser, selectUser, insertLivro, insertEmp, insertLigacao}
--- index.js
+++ index.js
@@ -8,8 +8,8 @@
   const entrada = require('prompt-sync')({sigint: true});
     
   //inserindo um usuário
-  console.log("Vamos inserir um usuario!");
-  var cpf = entrada('Digite o cpf(somente números): ');
+  console.log("Vamos cadastrar um usuario!");
+  var cpf = entrada("Digite o cpf (somente números): ");
   var nome = entrada("Digite o nome: ");
   var email = entrada("Digite o email: ");
   var endereco = entrada("Digite o endereco: ");
@@ -21,5 +21,27 @@
   var cpf_select = entrada("Digite um CPF: ");
   var user = await db.selectUser(cpf_select);    
   console.log(user);
+
+  //inserindo um livro
+  console.log("Vamos cadastrar um livro!");
+  var titulo = entrada("Digite o titulo: ");
+  var autor = entrada("Digite o autor: ");
+  var user_cpf = entrada("Digite o cpf do dono: ");
+  await db.insertLivro({titulo: titulo, autor:  autor, user_cpf: user_cpf});
+
+  //inserindo um emprestimo
+  console.log("Vamos fazer um empréstimo!");
+  var retirada = entrada("Digite a data e a hora da retirada (YYYY-MM-DD hh:mm:ss): ");
+  var devolucao = entrada("Digite a data e a hora da devolução (YYYY-MM-DD hh:mm:ss): ");
+  var user_cpf = entrada("Digite o cpf do realizador: ");
+  await db.insertEmp({retirada: retirada, devolucao:  devolucao, user_cpf: user_cpf});
+
+  //inserido a ligação entre o pedido do emprestimo e qual livro será
+  var idEMP = entrada("Digite o código do empréstimo: ");
+  var cpfEMP = entrada("Digite o cpf do realizador: ");
+  var idLIVRO = entrada("Digite o código do livro: ");
+  var cpfLIVRO = entrada("Digite o cpf do dono do livro: ");
+  await db.insertLigacao({idEMP: idEMP, cpfEMP:  cpfEMP, idLIVRO: idLIVRO, cpfLIVRO: cpfLIVRO});
+
   
 })();
